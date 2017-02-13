c40d42d27cd5a48f751bc9b2afa8dc9657257e87
Kevin Atkinson <k@kevina.org>
2017-02-13 15:56:35 -0500

    filestore util: refactor and clean up tests
    License: MIT
    Signed-off-by: Kevin Atkinson <k@kevina.org>

--- test/sharness/t0271-filestore-utils.sh
+++ test/sharness/t0271-filestore-utils.sh
@@ -8,6 +8,17 @@ test_description="Test out the filestore nocopy functionality"
 
 . lib/test-lib.sh
 
+test_init_filestore() {
+	test_expect_success "clean up old node" '
+		rm -rf "$IPFS_PATH" mountdir ipfs ipns
+	'
+
+	test_init_ipfs
+
+	test_expect_success "enable filestore config setting" '
+		ipfs config --json Experimental.FilestoreEnabled true
+	'
+}
 
 test_init_dataset() {
 	test_expect_success "create a dataset" '
@@ -17,6 +28,11 @@ test_init_dataset() {
 		random   10000  2 > somedir/file2 &&
 		random 1000000  3 > somedir/file3
 	'
+}
+
+test_init() {
+	test_init_filestore
+	test_init_dataset
 }
 
 EXPHASH="QmRueCuPMYYvdxWz1vWncF7wzCScEx4qasZXo5aVBb1R4V"
@@ -68,11 +84,16 @@ test_filestore_adds() {
 	'
 }
 
-test_filestore_verify() {
+# check that the filestore is in a clean state
+test_filestore_state() {
 	test_expect_success "ipfs filestore verify' output looks good'" '
 		ipfs filestore verify | LC_ALL=C sort > verify_actual
 		test_cmp verify_expect verify_actual
 	'
+}
+
+test_filestore_verify() {
+	test_filestore_state
 
 	test_expect_success "'ipfs filestore verify HASH' works" '
 		ipfs filestore verify $FILE1_HASH > verify_actual &&
@@ -113,16 +134,19 @@ test_filestore_verify() {
 		ipfs filestore verify > verify_actual &&
 		grep changed verify_actual | grep -q somedir/file3
 	'
+
+        # reset the state for the next test
+        test_init_dataset
 }
 
-cat <<EOF > dups_expect
-$FILE1_HASH
-EOF
+test_filestore_dups_and_rm() {
+	# make sure the filestore is in a clean state
+	test_filestore_state
 
-test_filestore_dups_rm() {
 	test_expect_success "'ipfs filestore dups'" '
 		ipfs add --raw-leaves somedir/file1 &&
 		ipfs filestore dups > dups_actual &&
+		echo "$FILE1_HASH" > dups_expect
 		test_cmp dups_expect dups_actual
 	'
 
@@ -149,36 +173,23 @@ test_filestore_dups_rm() {
 	'
 }
 
-init_ipfs_filestore() {
-	test_expect_success "clean up old node" '
-		rm -rf "$IPFS_PATH" mountdir ipfs ipns
-	'
+#
+# No daemon
+#
 
-	test_init_ipfs
-
-	test_expect_success "enable filestore config setting" '
-		ipfs config --json Experimental.FilestoreEnabled true
-	'
-}
-
-test_init_dataset
-
-init_ipfs_filestore
+test_init
 
 test_filestore_adds
 
 test_filestore_verify
 
-test_filestore_dups_rm
+test_filestore_dups_and_rm
 
-echo "WORKING DIR"
-echo "IPFS PATH = " $IPFS_PATH
-pwd
+#
+# With daemon
+#
 
-
-test_init_dataset
-
-init_ipfs_filestore
+test_init
 
 # must be in offline mode so tests of retrieving non-exist blocks
 # don't hang
@@ -188,7 +199,7 @@ test_filestore_adds
 
 test_filestore_verify
 
-test_filestore_dups_rm
+test_filestore_dups_and_rm
 
 test_kill_ipfs_daemon
 
